b17bc1e8eedee88f9adea5aff8002eb56310f6be
Wang Chi <hiwangchi@gmail.com>
2016-05-03 22:51:33 +0800

    initial

---
+++ scripts/components/Login.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>Welcome to login.</div>
+    );
+  };
+};
+
+export default Login;
